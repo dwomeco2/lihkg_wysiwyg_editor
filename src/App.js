bd5e45e8f460d8f4d09d0d46e4e19dd6f5ee94a0
kenny lam <kennylamwk@gmail.com>
2019-08-21 15:37:12 +0800

    add iframe version

--- src/App.js
+++ src/App.js
@@ -19,10 +19,16 @@ function App() {
             <h2>Security tips</h2>
           </header>
           <article>
-              <p>Make sure to confirm the lihkg account number also with the URL on this website.</p>
-              <pre>
-                Lihkg #no: #61985
-              </pre>
+            <p>Make sure to confirm the lihkg account number, App ID also with the URL on this website.</p>
+            <pre>
+              Lihkg #no: #61985 <br />
+              App ID: kajbhaokhejchokeeaeghickdcgpghde
+            </pre>
+            <br />
+            <p>
+              The iframe version is open source, people who concerns their privacy can use this instead. However, auto save function would not work in this version due to browser limitation. <br />
+              Repo: <a href="https://github.com/dwomeco2/lihkg_wysiwyg_editor_extension_iframe">lihkg_wysiwyg_editor_extension_iframe</a>
+            </p>
           </article>
         </section>
         <section>
@@ -33,11 +39,12 @@ function App() {
             <p>Follow the below steps to add the extension to your Google Chrome browser:</p>
             <ol className={"leading_tab"}>
               <li>Download the extension crx file <a href="./lihkg_wysiwyg_editor.crx">here</a></li>
+              <li>Iframe version of extension <a href="./lihkg_wysiwyg_editor_iframe.crx">here</a></li>
               <li>Go to the extension manager page chrome://extensions</li>
               <li>Drag and Drop the extension crx file to your chrome browser</li>
               <li>Confirm to install the extension</li>
-              <li>Confirm the app id <code>kajbhaokhejchokeeaeghickdcgpghde</code> in extension manager page</li>
-              <li>You are all-set now, head over to lihkg.com, a transparent toolbar appears at the bottom left borner<img alt="minimized_ext" src="https://imgur.com/eczddHD.png"/></li>
+              <li>Confirm the app id <code>kajbhaokhejchokeeaeghickdcgpghde</code> in extension manager page only if you are not using iframe version</li>
+              <li>You are all-set now, head over to lihkg.com, a transparent toolbar appears at the bottom left borner <br /><img alt="minimized_ext" src="https://imgur.com/eczddHD.png"/></li>
               <li>Enjoy keyboard fighting!</li>
             </ol>
             <p>
